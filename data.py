9edd5a1df009ae5f6a3c4d3b00df04e10e4e0990
Neo-Nguyen-95 <dzung.usth@gmail.com>
2025-04-20 00:51:34 +0700

    basically finish it

--- data.py
+++ data.py
@@ -1,29 +1,65 @@
 import pandas as pd
 pd.set_option("display.max_columns", None)
 import os
+import re
+import string
 
-#%% DATA LOADING
+#%%
 
-def load_data():
-    file_list = os.listdir("data")
-    result = pd.DataFrame()
+class DataRepository:
+    def __init__(self):
+        pass
     
-    for file_name in file_list:
-        file_path = "data/" + file_name
-        df = pd.read_csv(file_path)
-
-        name_split = file_name.split(".")[0].split("_")
-        cat = name_split[0]
-        topic = "_".join(name_split[1:])
-
-        df['cat'] = cat
-        df['topic'] = topic
+    #%% DATA LOADING
+    
+    def load_data(self):
+        file_list = os.listdir("data")
+        result = pd.DataFrame()
         
-        result = pd.concat([result, df])
+        for file_name in file_list:
+            #---
+            file_path = "data/" + file_name
+            df = pd.read_csv(file_path)
+            
+            # transform name => category + topic
+            name_split = file_name.split(".")[0].split("_")
+            cat = name_split[0]
+            topic = "_".join(name_split[1:])
+            df['cat'] = cat
+            df['topic'] = topic
+            
+            #---
+            result = pd.concat([result, df])
+    
+        result = result.reset_index(drop=True)
+        return self.clean_data(result)
 
-    result = result.reset_index(drop=True)
-    return result
 
-df = load_data()
+    #%% DATA CLEANING (SUPPORT FUNCTION)
+    
+    def clean_data(self, df):
+        # 1. make lowercase
+        df['content'] = df['content'].apply(lambda x: x.lower())
 
-#%% DATA CLEANING
+        # 2. remove punctuation, new line char, ... char, digit
+        remove_str = "[" + re.escape(string.punctuation) + "\n" + "…" + "]+"  + "|" + r"\d+"
+
+        df['content'] = df['content'].apply(
+            lambda x: re.sub(remove_str, " ", x)
+            )
+
+        # clean up multiple space
+        df['content'] = df['content'].apply(
+            lambda x: re.sub(r"\s+", " ", x).strip()
+            )
+
+        # 3. tokenization
+        df['content'] = df['content'].apply(
+            lambda x: x.split(" ")
+            )
+        
+        return df
+
+
+
+
---
+++ main.py
@@ -0,0 +1,114 @@
+from data import DataRepository
+import numpy as np
+import pandas as pd
+
+#%% PREP   
+ 
+# Load data
+repo = DataRepository()
+df = repo.load_data()
+
+# Train, Test data
+df_train = df[df['cat']=='training']
+df_test = df[df['cat']=='testing']
+
+categories = df_train['topic'].unique()
+
+# Get size of vocabulary
+def get_vocab(ds):
+    vocab = []
+    for x in ds:
+        vocab.extend(x)
+        
+    return vocab
+
+def get_vocab_size(vocab, bank_type=None):
+    if bank_type == "unique":
+        vocab = set(vocab)
+    
+    return len(vocab)
+
+# Get word count in a list
+def count_word(vocab, vocab_train=None):
+    
+    word_count = {}
+    if vocab_train:
+        for word in set(vocab_train):
+            word_count[word] = 0
+    else:
+        for word in set(vocab):
+            word_count[word] = 0
+        
+    for word in vocab:
+        word_count[word] += 1
+        
+    return word_count
+
+# Estimate word prob (Step 3)
+def cal_word_prob(Nci, Nc, V_size):
+    alpha = 1  # As Laplace smoothing
+    P_word_category = (Nci + alpha) / (Nc + alpha * V_size)
+    
+    return P_word_category
+
+#%% TRAINING
+# Step 1. Build vocabulary
+vocab_train = get_vocab(df_train['content'])
+V_size = get_vocab_size(vocab_train, bank_type="unique")
+
+report = {cat: {} for cat in categories}
+
+posterior_cat = {cat: [] for cat in categories}
+
+for cat in categories:
+    report[cat]['vocab'] = get_vocab(df_train[df_train['topic']==cat]['content'])
+    report[cat]['count'] = count_word(report[cat]['vocab'], vocab_train)
+    
+# Step 2. Total word in each catetory
+    report[cat]['Nc'] = get_vocab_size(report[cat]['vocab'])
+    
+# Step 4. Prior probability of category
+    report[cat]['prior'] = (
+        len(df_train[df_train['topic']==cat])/len(df_train)
+          
+         )
+    
+#%% TESTING
+    # Step 5. Test data with word count
+    
+    for testing_vocab in df_test['content']:
+    
+        testing_count = count_word(testing_vocab)
+    
+        # starting for posterior probability
+        posterior = report[cat]['prior']
+        
+        for word in testing_count.keys():
+            if word in set(vocab_train):
+                Nci = report[cat]['count'][word]
+                Nc = report[cat]['Nc']
+                
+                # update posterior    
+                posterior *= (cal_word_prob(Nci, Nc, V_size) ** testing_count[word])
+         
+        log_posterior = np.log(posterior)
+        
+        posterior_cat[cat].append(log_posterior)
+
+pred = []
+
+for n in range(len(df_test)):
+    # initialization of argmax value
+    argmax = categories[0]
+    
+    for cat in categories:
+        if posterior_cat[cat][n] > posterior_cat[argmax][n]:
+            argmax = cat
+            
+    pred.append(argmax)
+    
+df_test['pred'] = pred
+
+acc = (df_test['topic'] == df_test['pred']).sum() / len(df_test)
+        
+print(f"Accuracy: {acc:.0%}")
