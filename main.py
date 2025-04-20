c6480a5e166895c9b5ae6e91b74e7f28310ad293
Neo-Nguyen-95 <dzung.usth@gmail.com>
2025-04-20 15:36:47 +0700

    update OOP ver

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 from data import DataRepository
+from naive_bayes import NaiveBayesClassifier
 import numpy as np
 import pandas as pd
 
-#%% PREP   
+#%% NEW VERSION
+# Load data
+repo = DataRepository()
+df = repo.load_data()
+
+# Train, Test data
+df_train = df[df['cat']=='training']
+df_test = df[df['cat']=='testing']
+
+# Load Model
+model = NaiveBayesClassifier()
+
+#%% TESTING
+model.fit(df_train['content'].values, df_train['topic'].values)
+
+pred = []
+for content in df_test['content']:
+    pred.append(model.predict(content))
+    
+df_test['pred'] = pred
+
+acc = (df_test['topic'] == df_test['pred']).sum() / len(df_test)
+        
+print(f"Accuracy: {acc:.0%}")
+
+#%% PREP (OLD VERSION)
  
 # Load data
 repo = DataRepository()
---
+++ naive_bayes.py
@@ -0,0 +1,104 @@
+import numpy as np
+
+
+class NaiveBayesClassifier:
+    #%% INIT
+    def __init__(self, alpha: float = 1.0):
+        self.alpha = alpha
+        
+    #%% 
+    def fit(self, X: list[list[str]], C: list[str]) -> None:
+        """Learn:
+            - general vocab & vocab size
+            - Nc (each class)
+            - priors (each class)
+        """
+        ### 0. Result placement
+        class_trained = {
+            c: {
+                'vocab': [],
+                'count': {},
+                'Nc': 0,
+                'prior': 0
+                } for c in C
+            }
+        
+        ### 1. Build general vocab
+        vocab_train = self.get_vocab(X, nested=True)
+        V_size = len(set(vocab_train))
+        
+        # Create vocab of each class
+        for x, c in zip(X, C):
+            class_trained[c]['vocab'].extend(self.get_vocab(x, nested=False))
+            
+            ### 4. Compute class prior
+            class_trained[c]['prior'] += 1/len(C) 
+        
+        
+        for c in set(C):
+            # Count word
+            class_trained[c]['count'] = self.count_word(class_trained[c]['vocab'])
+            
+            ### 2. Count feature frequency
+            class_trained[c]['Nc'] = len(class_trained[c]['vocab'])
+        
+        self.class_trained = class_trained
+        self.V_size = V_size
+        
+    # Support Function    
+    def get_vocab(self, 
+                  X: list[list[str]] | list[str],
+                  nested: bool = True
+                  ) -> list[str]:
+        
+        vocab = []
+        if nested:
+            for x in X:
+                vocab.extend(x)
+        else:
+            for x in X:
+                vocab.append(x)
+            
+        return vocab
+    
+    def count_word(self, vocab: list[str]) -> int:
+        
+        word_count = {}
+        unique_vocab = set(vocab)
+        
+        for word in unique_vocab:
+            word_count[word] = 0
+        
+        for word in vocab:
+            word_count[word] += 1
+            
+        return word_count
+          
+    #%%
+    def predict(self, X_test: list[str]) -> list[str]:
+        # Get list of class from training data
+        C = self.class_trained.keys()
+        
+        # Initiate posterior with prior value
+        posterior_log_proba = {
+            c: np.log(self.class_trained[c]['prior']) for c in C
+            }
+        
+        # Create vocab of testing item
+        x_vocab = self.get_vocab(X_test, nested = False)  # -> list
+        
+        for word in x_vocab:
+            for c in C:
+                if word in self.class_trained[c]['vocab']:
+                    Nci = self.class_trained[c]['count'][word]
+                else:
+                    Nci = 0
+                
+                posterior_log_proba[c] += np.log(
+                    (Nci + self.alpha) / 
+                    (self.class_trained[c]['Nc'] + self.V_size)
+                    )
+                
+        return max(posterior_log_proba, key = posterior_log_proba.get)
+        
+        
